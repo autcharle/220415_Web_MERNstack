d1e1bb76fb75cef5335e528bde1279d8af143be6
TT <3t.charle@gmail.com>
2022-04-20 09:25:47 +0700

    generate web token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,6 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user.id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id),
         })
     }
     else{
@@ -62,6 +63,7 @@ const loginUser = asyncHandler(async (req, res) => {
             _id: user.id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id),
         })
     } else{
         res.status(400)
@@ -75,6 +77,13 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
     res.json({ message: 'User data display' })
 })
+
+// Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
 
 module.exports = {
     registerUser,
